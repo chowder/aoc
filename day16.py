498d74fb46aaac7138df37467a7f8d354574de06
chowder <16789070+chowder@users.noreply.github.com>
2022-12-17 01:48:02 +0000

    day16 washing

--- day16.py
+++ day16.py
@@ -16,6 +16,22 @@ def min_distance(adj_dict, start, end):
         queue.extend([(adj_node, distance + 1) for adj_node in adj_dict[node]])
 
 
+def traverse(nodes, dist, rates, start, seen, minute, rate, max_time, memo):
+    if m := memo.get(key := (start, seen, minute)):
+        return m
+
+    max_released = (max_time - minute + 1) * rate
+    new_seen = seen | {start}
+
+    for node in nodes - seen:
+        if node != start and minute + (d := dist[start, node] + 1) <= max_time:
+            released = traverse(nodes, dist, rates, node, new_seen, minute + d, rate + rates[node], max_time, memo) + rate * d
+            max_released = max(max_released, released)
+
+    memo[key] = max_released
+    return max_released
+
+
 def main():
     with open("inputs/day16.txt") as f:
         lines = [re.findall(r"([A-Z]{2}|\d+)", line) for line in f.read().splitlines()]
@@ -23,26 +39,16 @@ def main():
         rates = {v[0]: int(v[1]) for v in lines}
         dist = {(a, b): min_distance(adj, a, b) for a, b in product(adj.keys(), repeat=2) if a != b}
 
-        def traverse(nodes, start, seen, minute, rate, released, max_time):
-            nonlocal dist, rates
-
-            max_released = released + (max_time - minute + 1) * rate
-            for node in nodes - seen:
-                if node != start and minute + (d := dist[start, node] + 1) <= max_time:
-                    max_released = max(max_released, traverse(nodes, node, seen | {start}, minute + d, rate + rates[node], released + rate * d, max_time))
-
-            return max_released
-
         # Silver
-        nodes = frozenset([n for n in adj.keys() if rates[n] > 0])
-        print(traverse(nodes, "AA", frozenset(), 1, 0, 0, 30))
+        nodes = set([n for n in adj.keys() if rates[n] > 0])
+        print(traverse(nodes, dist, rates, "AA", frozenset(), 1, 0, 30, {}))
 
         # Gold
         max_release = 0
-        splits = [(frozenset(c), nodes - set(c)) for i in range(1, (len(nodes) // 2) + len(nodes) % 2 + 1) for c in combinations(nodes, i)]
+        splits = [(set(c), nodes - set(c)) for i in range(1, (len(nodes) // 2) + 1) for c in combinations(nodes, i)]
         for mine, elephants in splits:
-            mine = traverse(mine, "AA", frozenset(), 1, 0, 0, 26)
-            elephants = traverse(elephants, "AA", frozenset(), 1, 0, 0, 26)
+            mine = traverse(mine, dist, rates, "AA", frozenset(), 1, 0, 26, {})
+            elephants = traverse(elephants, dist, rates, "AA", frozenset(), 1, 0, 26, {})
             max_release = max(max_release, mine + elephants)
 
         print(max_release)
