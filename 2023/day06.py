dac5644499ee05bafe116509d55100da9ff8ec50
chowder <16789070+chowder@users.noreply.github.com>
2023-12-06 22:53:01 +0000

    day 6 tidying

--- 2023/day06.py
+++ 2023/day06.py
@@ -1,4 +1,6 @@
 import math
+from functools import reduce
+from operator import mul
 
 
 def ways(t, d):
@@ -15,42 +17,12 @@ def main():
     with open("inputs/day06.txt") as f:
         lines = f.read().splitlines()
 
-    times = [int(t) for t in lines[0].split(":")[1].split(" ") if t]
-    distances = [int(d) for d in lines[1].split(":")[1].split(" ") if d]
+    times = [t for t in lines[0].split(":")[1].split(" ") if t]
+    distances = [d for d in lines[1].split(":")[1].split(" ") if d]
 
-    # Silver
-    silver = 1
-    for t, d in zip(times, distances):
-        silver *= ways(t, d)
-
-    print(silver)
-
-    # Gold
-    time = int(lines[0].split(":")[1].replace(" ", ""))
-    distance = int(lines[1].split(":")[1].replace(" ", ""))
-
-    print(ways(time, distance))
+    print(reduce(mul, [ways(int(t), int(d)) for t, d in zip(times, distances)]))  # Silver
+    print(ways(int("".join(times)), int("".join(distances))))  # Gold
 
 
 if __name__ == "__main__":
     main()
-"""
-# t = 7
-#
-# h <= t
-#
-# h * (t - h) > d
-# - h^2 + ht > d
-# - h^2 + ht - d > 0
-
-# -t [+-] sqrt(t^2 - 4(-1)(-d)) / 2 * (-1)
-# -t [+-] sqrt(t^2 - 4d) / (-2)
-
-t + sqrt(t^2 - 4d) / 2
-t - sqrt(t^2 - 4d) / 2
-
-
-
-# t^2 - 2d = 0
-# t = sqrt(2d)
-"""
